8a1c9dacb5f851de4909f53a23bb9eef887c222d
EmilioPFematt <supermageblaziken@gmail.com>
2022-11-28 14:51:20 -0600

    Avance-1

--- MESA/Agents/agents.py
+++ MESA/Agents/agents.py
@@ -1,5 +1,6 @@
 import mesa
 
+
 class Car(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
@@ -8,28 +9,31 @@ class Car(mesa.Agent):
         self.move()
     
     def move(self):
-        pos_adj = self.model.grid.get_neighborhood(
-            self.pos,
-            moore = False,
-            include_center = False
-        )
-        neighbors = [i.pos for i in self.model.grid.get_neighbors(self.pos, False)]
-        val = [i for i in pos_adj if i not in neighbors]
-        new_position = self.pos
-        if(len(val) != 0):
-            new_position = self.random.choice(val)
-        self.model.grid.move_agent(self, new_position)
+        #pos_adj = self.model.grid.get_neighborhood(
+        #   self.pos,
+        #    moore = True,     
+        #    include_center = False
+        #)
+        #print(self.pos,pos_adj)
+        #val = [i for i in pos_adj if i not in neighbors]
+        new_position = (self.pos[0], (self.pos[1]+1)%self.model.grid.height)
+        if(self.model.grid.is_cell_empty(new_position)):
+            self.model.grid.move_agent(self, new_position)
+        
+        #print(self.pos)
 
+        
 class Inteseccion(mesa.Model):
     def __init__(self, N, width, height):
+        #print(width)
+        #print(height)
         self.num_agents = N
-        self.grid = mesa.space.Grid(width, height, False)
+        self.grid = mesa.space.SingleGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
-
         for i in range(self.num_agents):
             a = Car(i, self)
             self.schedule.add(a)
-            self.grid.place_agent(a, (self.random.randrange(width), self.random.randrange(height)))
+            self.grid.position_agent(a, "random", "random")
         
     def step(self):
         self.schedule.step()
--- MESA/main.py
+++ MESA/main.py
@@ -1,11 +1,26 @@
 from Agents.agents import Inteseccion
+import mesa
+def agent_portrayal(agent, shape = "circle", color = "red", filled = True, size = 0.3):
+    colors = ["red", "blue", "green"]
+    portrayal = {
+        "Shape":shape,
+        "Color":colors[agent.unique_id%3],
+        "Filled":filled,
+        "Layer":0,
+        "r":size
+    }
+    return portrayal
 
 def main():
-    model = Inteseccion(5, 1, 10)
-    print([x.pos for x in model.schedule.agents])
-    for i in range(10):
-        model.step()
-        print([x.pos for x in model.schedule.agents])
-
+    #model = Inteseccion(5, 1, 10)
+    #for i in range(5):
+    #    model.step()
+    grid = mesa.visualization.CanvasGrid(agent_portrayal, 1, 10, 50, 500)
+    server = mesa.visualization.ModularServer(
+        Inteseccion, [grid], "Rotonda", {"N":6, "width":1, "height":10}
+    )
+    server.port = 8522
+    server.launch()
+    
 if __name__ == "__main__":
     main()
